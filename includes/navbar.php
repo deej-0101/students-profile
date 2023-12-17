52ec5769ffd55b3e2e31522d3860d0ec70ced2e6
deej-0101 <diane.joy.d.n@gmail.com>
2023-12-18 00:39:06 +0800

    edit nav bar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,16 +1,16 @@
 <div class="sidebar">
         <h1>Menu</h1>
         <ul>
-            <li><a href="/index.php">Home</a></li>
-            <li><a href="views/students.view.php">Students</a></li>
-            <li><a href="/town_view.php">Town</a></li>
-            <li><a href="/province.view.php">Province</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="students.view.php">Students</a></li>
+            <li><a href="town_view.php">Town</a></li>
+            <li><a href="province.view.php">Province</a></li>
             <li class="dropdown">
                 <a href="#" class="dropbtn">Reports</a>
                 <div class="dropdown-content">
-                    <a href="#">Report 1</a>
-                    <a href="#">Report 2</a>
-                    <a href="#">Report 3</a>
+                    <a href="report1.php">Report 1</a>
+                    <a href="report2.php">Report 2</a>
+                    <a href="report3.php">Report 3</a>
                 </div>
             </li>
         </ul>
